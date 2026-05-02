e8fd9aea93789d6b60662dfa241c4e75c0ab9e2f
Abdelrahman Sabry <39697434+Sabryy0@users.noreply.github.com>
2026-05-03 00:05:36 +0300

    Add FoodServices index file to export services

---
+++ React_frontend/src/services/FoodServices/index.js
@@ -0,0 +1,5 @@
+// Export all food service modules
+export { mealService } from './mealService';
+export { recipeService } from './recipeService';
+export { mealSuggestionService } from './mealSuggestionService';
+export { leftoverService } from './leftoverService';
